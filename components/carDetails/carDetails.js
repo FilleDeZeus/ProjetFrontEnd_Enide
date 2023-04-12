41e23be0650e6407d9aca7a5174ae6f36f2a5f64
Enide Djender <enidedjender@MacBook-Air-de-Enide.local>
2023-04-12 19:26:48 +0200

    map good

--- components/carDetails/carDetails.js
+++ components/carDetails/carDetails.js
@@ -1,11 +1,18 @@
+import { Footer } from '../footer/footer';
 import styles from './public/sass/carDetails.module.scss';
 
 export const CarDetails = ({ car }) => {
-  const googleMapsUrl = `https://www.google.com/maps/embed/v1/view?key=YOUR_API_KEY&center=${car.latitude},${car.longitude}&zoom=17`;
+  const googleMapsUrl = `https://www.google.com/maps/embed/v1/view?key=AIzaSyBhYZkQeGxy5n8UXnNF0P9V9fiWuGG4_ps&center=${car.latitude},${car.longitude}&zoom=17`;
 
-  
   return (
-    <div className={styles.carDetails}>
+    <div>
+    <div className={styles.carDetails}
+    style={{
+      backgroundImage: car.image ? `url(${car.image})` : '',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+    }}>
       <h1 className={styles.carTitle}>{car.make_id} {car.model}</h1>
       <div className={styles.carImageContainer}>
         <img className={styles.carImage} src={car.image} alt={car.model} />
@@ -16,15 +23,29 @@ export const CarDetails = ({ car }) => {
           <li><span>Couleur:</span> {car.color}</li>
           <li><span>Kilométrage:</span> {car.mileage}</li>
           <li><span>Prix:</span> {car.price}</li>
-          <li>
-            <a href={googleMapsUrl} target="_blank" rel="noopener noreferrer">
-              Voir sur Google Maps
+          
+        </ul>
+        <p className={styles.carDescription}>{car.description}</p>
+        <div className={styles.googleMapsContainer}>
+        <a href={googleMapsUrl} target="_blank" rel="noopener noreferrer">
+          <iframe
+            title="Google Maps"
+            className={styles.googleMaps}
+            src={googleMapsUrl}
+            width="600"
+            height="450"
+            frameBorder="none"
+            allowFullScreen=""
+            aria-hidden="false"
+            tabIndex="0"
+          ></iframe>
             </a>
-          </li>
-          </ul>
-          <p className={styles.carDescription}>{car.description}</p>
-          </div>
-          </div>
-);
-};
 
+        </div>
+      </div>
+    </div>
+    <Footer/>
+
+    </div>
+  );
+};
